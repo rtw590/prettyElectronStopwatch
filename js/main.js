33b0ccf6e6bec406f9fff9c00d23eda84c1a6bda
Rob Laptop <r.t.waller@gmail.com>
2018-01-15 18:33:30 -0600

    fully functionally. Not electron app yet.

--- js/main.js
+++ js/main.js
@@ -25,17 +25,49 @@ var saturation = '76%';
 var lightness = '53%';
 
 
-
-if (timer.innerText === '00 : 00') {
+if (timer.innerText.slice(-2) === '00') {
     var hueChange = (parseInt(hueStart) - 20);
 } 
 
-if (timer.innerText === '00 : 05') {
-    var hueChange = (parseInt(hueStart) - 20);
-} 
 
 var bk = document.getElementById('bodyBox');
 
 bk.style.backgroundColor = 'hsl('+hueChange+', '+saturation+', '+lightness+')';
+
+function changeOnThirty () {
+	console.log(hueChange);
+	hueChange = (parseInt(hueChange) - 53);
+	bk.style.backgroundColor = 'hsl('+hueChange+', '+saturation+', '+lightness+')';
+	console.log(hueChange);
+	// console.log('It Happened');
+}
+
+function isThirty() {
+	// console.log('It Happened');
+    if (timer.innerText.slice(-2) === '30') {
+		changeOnThirty();
+	} 
+}
+
+setInterval(isThirty, 1000);
+
+function changeOnDoubleZero () {
+	console.log(hueChange);
+	hueChange = (parseInt(hueChange) - 53);
+	bk.style.backgroundColor = 'hsl('+hueChange+', '+saturation+', '+lightness+')';
+	console.log(hueChange);
+	// console.log('It Happened');
+}
+
+function isDoubleZero() {
+	// console.log('It Happened');
+    if (timer.innerText.slice(-2) === '00') {
+		changeOnDoubleZero();
+	} 
+}
+
+setInterval(isDoubleZero, 1000);
+
+
 
 
